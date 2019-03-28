69a590cdede866b2d0f5ea0f6b9343f3070074e5
john-weetaker <jjdormevil@gmail.com>
2019-03-28 16:49:43 +0100

    functional osgi plugin demo

--- src/main/java/com/flocondria/greet/Greeting.java
+++ src/main/java/com/flocondria/greet/Greeting.java
@@ -4,13 +4,20 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Greeting implements BundleActivator {
+
     @Override
     public void start(BundleContext context) throws Exception {
         System.out.println("The bundle is starting");
+        greet();
+        //throw new Exception("Failing load plugin");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         System.out.println("The bundle is stopping");
+    }
+
+    public void greet() {
+        System.out.println("Greetinnnnnnnnnnnng !!! yato");
     }
 }
